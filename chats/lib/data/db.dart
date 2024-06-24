2e95d96a9ac73131fedb8a1139327697c6666476
Kirill Mironov <kirill.mironov@gmail.com>
2024-06-24 20:21:16 +0300

    move chats to docker

--- chats/lib/data/db.dart
+++ chats/lib/data/db.dart
@@ -3,13 +3,7 @@ import 'package:stormberry/stormberry.dart';
 late Database db;
 
 void initDatabase() {
-  db = Database(
-    host: 'localhost',
-    port: 4501,
-    useSSL: false,
-    username: 'kirimi',
-    password: 'kirimi',
-  );
+  db = Database();
   db.debugPrint = true;
   db.open();
 }
